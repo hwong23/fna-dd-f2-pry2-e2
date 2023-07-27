fb1ae357525954c6e943227b606509890bbb8ada
harry.wong <hwong23@gmail.com>
2023-07-27 13:00:48 -0500

    nombres--prdt

--- build/build.sh
+++ build/build.sh
@@ -28,7 +28,7 @@ COMMIT="${TRIGGERING_SHA_7}"
 # located in the $PANDOC_DATA_DIR/defaults directory.
 PANDOC_DATA_DIR="${PANDOC_DATA_DIR:-build/pandoc}"
 export FECHA_COMPILACION COMMIT PROYECTO PROY_DESCR
-export PR06_NOMBRE PR07_NOMBRE
+export PR013_NOMBRE
 
 # Generate reference information
 echo >&2 "Retrieving and processing reference metadata"
